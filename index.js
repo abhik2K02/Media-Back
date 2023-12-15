0a1ea283293d8607b8eff0690d4bd267215d49f1
abhik2K02 <abhishekym4@gmail>
2023-12-15 13:04:03 +0530

    mdback

---
+++ index.js
@@ -0,0 +1,39 @@
+// import json-server using require method
+
+const jsonserver=require('json-server') 
+
+
+// create server
+
+const server=jsonserver.create()
+
+
+const router=jsonserver.router('db.json')
+
+
+
+
+
+// create middlware
+
+const middleware=jsonserver.defaults()
+
+
+// set port for server 
+
+const PORT=process.env.PORT||4000
+
+
+// use middleware in server
+
+server.use(middleware)
+
+server.use(router) 
+
+
+
+// to work server
+
+server.listen(PORT,()=>{
+    console.log('media player server started at port:'+PORT);
+})
